04473c575a787910d88bd448369d07e4ac4ebdad
Nickolas Burr <nickolasburr@gmail.com>
2017-10-07 15:49:34 -0400

    Fix cflags variable

--- free.rb
+++ free.rb
@@ -4,9 +4,9 @@ class Free < Formula
   url "https://github.com/dcantrell/darwin-free/archive/v0.5.1.tar.gz"
   sha256 "3ecb8624898daec1d7968ba420f5c71b74b6306e1dc3038b9862fe64e9e6fcd0"
 
-  cflags = %q(-O2 -Wall -std=c99 -D_FREE_VERSION="\"${VER}\"")
-
   def install
+    cflags = %q(-O2 -Wall -std=c99 -D_FREE_VERSION="\"${VER}\"")
+
     system "make CFLAGS='#{cflags}'"
 
     bin.install "free"
