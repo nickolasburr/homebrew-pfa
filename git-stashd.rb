31d7b457b0d1a05bd59fbd2116d9bc3229aa5246
Nickolas Burr <nickolasburr@gmail.com>
2017-10-07 16:01:32 -0400

    Add git-stashd formula

---
+++ git-stashd.rb
@@ -0,0 +1,21 @@
+class GitStashd < Formula
+  desc "Git autostashing daemon."
+  homepage "https://github.com/nickolasburr/git-stashd"
+  url "https://github.com/nickolasburr/git-stashd/archive/1.0.2.tar.gz"
+  sha256 "bd7f6ebb3c4ed65f7af518762bc00dd8c448d0b58bc45dcea19d3f85145c972e"
+
+  depends_on "openssl" => :required
+  depends_on "cmake" => :required
+
+  def install
+    system "make"
+
+    bin.install "git-stashd"
+    man1.install "man/git-stashd.1.gz"
+  end
+
+  test do
+    system "git", "init"
+    system "git", "stashd", "--help"
+  end
+end
