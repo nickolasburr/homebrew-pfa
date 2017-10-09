ab68367086a217f1aeab7108bd739c16bc1480fe
Nickolas Burr <nickolasburr@gmail.com>
2017-10-09 11:43:35 -0400

    Add bottle to git-stashd formula

--- git-stashd.rb
+++ git-stashd.rb
@@ -4,6 +4,11 @@ class GitStashd < Formula
   url "https://github.com/nickolasburr/git-stashd/archive/1.0.2.tar.gz"
   sha256 "bd7f6ebb3c4ed65f7af518762bc00dd8c448d0b58bc45dcea19d3f85145c972e"
 
+  bottle do
+    root_url "https://dl.bintray.com/nickolasburr/homebrew-bottles/git-stashd-1.0.2.sierra.bottle.tar.gz"
+    sha256 "5d99f1e81a38ed4031b9ffbf8defce0790ea0e2bc9d44ade41f84d9dc06260ff" => :sierra
+  end
+
   depends_on "openssl" => :required
   depends_on "cmake" => :required
 
