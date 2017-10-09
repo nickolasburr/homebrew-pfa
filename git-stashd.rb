d4ed88c57673abc1eaa6f175b993f41e8a914755
Nickolas Burr <nickolasburr@gmail.com>
2017-10-09 11:56:10 -0400

    Update bottle block in git-stashd formula

--- git-stashd.rb
+++ git-stashd.rb
@@ -3,10 +3,12 @@ class GitStashd < Formula
   homepage "https://github.com/nickolasburr/git-stashd"
   url "https://github.com/nickolasburr/git-stashd/archive/1.0.2.tar.gz"
   sha256 "bd7f6ebb3c4ed65f7af518762bc00dd8c448d0b58bc45dcea19d3f85145c972e"
+  version "1.0.2"
 
   bottle do
-    root_url "https://dl.bintray.com/nickolasburr/homebrew-bottles/git-stashd-1.0.2.sierra.bottle.tar.gz"
+    root_url "https://dl.bintray.com/nickolasburr/homebrew-bottles"
     sha256 "5d99f1e81a38ed4031b9ffbf8defce0790ea0e2bc9d44ade41f84d9dc06260ff" => :sierra
+    sha256 "5d99f1e81a38ed4031b9ffbf8defce0790ea0e2bc9d44ade41f84d9dc06260ff" => :el_capitan
   end
 
   depends_on "openssl" => :required
