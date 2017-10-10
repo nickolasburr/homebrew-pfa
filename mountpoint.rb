fd4cf33d8397b1e9d00416b0a26aa1d915a5b703
Nickolas Burr <nickolasburr@gmail.com>
2017-10-10 13:46:28 -0400

    Add bottles to mountpoint formula

--- mountpoint.rb
+++ mountpoint.rb
@@ -1,15 +1,25 @@
 class Mountpoint < Formula
-  desc "Linux mountpoint ported to Darwin"
-  homepage "https://github.com/nickolasburr/homebrew-ppf"
+  desc "See if a directory is a mountpoint."
+  homepage "http://download.savannah.gnu.org/releases/sysvinit/"
   url "http://download.savannah.gnu.org/releases/sysvinit/sysvinit-2.88dsf.tar.bz2"
   sha256 "60bbc8c1e1792056e23761d22960b30bb13eccc2cabff8c7310a01f4d5df1519"
+  version "2.88dsf"
+
+  bottle do
+    root_url "https://dl.bintray.com/nickolasburr/homebrew-bottles"
+    sha256 "6e7bda2ca5f2629acc7ac4c602347c76352de6064e684fa98a3d158e1dcd85b3" => :high_sierra
+    sha256 "6e7bda2ca5f2629acc7ac4c602347c76352de6064e684fa98a3d158e1dcd85b3" => :sierra
+    sha256 "6e7bda2ca5f2629acc7ac4c602347c76352de6064e684fa98a3d158e1dcd85b3" => :el_capitan
+    sha256 "6e7bda2ca5f2629acc7ac4c602347c76352de6064e684fa98a3d158e1dcd85b3" => :yosemite
+  end
 
   def install
     system "/usr/bin/sed -i '' 's@#include <sys/stat.h>@#include <sys/types.h>\\\n#include <sys/stat.h>@' src/mountpoint.c"
     system "/usr/bin/cc", "-Wno-everything", "src/mountpoint.c", "-o", "mountpoint"
+    system "/usr/bin/gzip -c man/mountpoint.1 > mountpoint.1.gz"
 
     bin.install "mountpoint"
-    man1.install "man/mountpoint.1"
+    man1.install "mountpoint.1.gz"
   end
 
   test do
