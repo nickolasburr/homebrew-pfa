e5b7c56afeb7b09a042f548feec7ce344f7227ac
Nickolas Burr <nickolasburr@gmail.com>
2017-10-10 19:19:56 -0400

    Add battery formula

---
+++ battery.rb
@@ -0,0 +1,14 @@
+class Battery < Formula
+  desc "Analyze information about your MacBook (Pro) battery."
+  homepage "https://github.com/nickolasburr/battery"
+  url "https://github.com/nickolasburr/battery/archive/1.0.0.tar.gz"
+  sha256 "60a90ceee563e79873f564d4edfaaba4d2f2a0974fda522da50b2cb924162117"
+
+  def install
+    bin.install "battery"
+  end
+
+  test do
+    system "battery", "--help"
+  end
+end
