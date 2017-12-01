7f223115a00ee030b318d97cccb10a159bdb2ad1
Nickolas Burr <nickolasburr@gmail.com>
2017-11-30 22:55:01 -0500

    Add explicit version to extract.rb

--- extract.rb
+++ extract.rb
@@ -3,6 +3,7 @@ class Extract < Formula
   homepage "https://gist.github.com/nickolasburr/c679c0ff8864660ef6fc702a23dcdbc8"
   url "https://gist.githubusercontent.com/nickolasburr/c679c0ff8864660ef6fc702a23dcdbc8/raw/d4447ea1489241b0df85ba35596d99e6d1f2bb4c/extract"
   sha256 "19221bfce1fe681632dab66d660e2ac2fa324f82a8e7f2d94621b3ec750ecaf0"
+  version "1.0.0"
 
   def install
     bin.install "extract"
