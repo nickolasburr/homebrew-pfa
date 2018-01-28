e3bb9a4af67e00fd7f524b66a1b3f7ac273f11d6
Nickolas Burr <nickolasburr@gmail.com>
2018-01-27 22:34:28 -0500

    Update git-follow formula

--- git-follow.rb
+++ git-follow.rb
@@ -3,14 +3,21 @@ class GitFollow < Formula
   homepage "https://github.com/nickolasburr/git-follow"
   url "https://github.com/nickolasburr/git-follow/archive/1.1.5.tar.gz"
   sha256 "d71bf48e92d4dc37505cd5427fc7fa2fcb9b4f0604e4ad593c10252675934dcb"
+  revision 1
 
   def install
-    bin.install "git-follow"
-    man1.install "git-follow.1.gz"
+    target = "git-follow"
+    module_path = "#{etc}/#{target}"
+
+    module_path.mkpath
+    module_path.install "src"
+
+    bin.install "#{target}"
+    man1.install "man/#{target}.1.gz"
   end
 
   test do
-    system "git", "init"
-    system "git", "follow", "--help"
+    system "git init"
+    system "git follow --version"
   end
 end
