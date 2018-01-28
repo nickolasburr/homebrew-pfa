87a374e9178e08bae12c484822da7f8ba6efe1cd
Nickolas Burr <nickolasburr@gmail.com>
2018-01-27 22:42:32 -0500

    Fix module install issue

--- git-follow.rb
+++ git-follow.rb
@@ -3,14 +3,13 @@ class GitFollow < Formula
   homepage "https://github.com/nickolasburr/git-follow"
   url "https://github.com/nickolasburr/git-follow/archive/1.1.5.tar.gz"
   sha256 "d71bf48e92d4dc37505cd5427fc7fa2fcb9b4f0604e4ad593c10252675934dcb"
-  revision 1
+  revision 2
 
   def install
     target = "git-follow"
-    module_path = "#{etc}/#{target}"
 
-    module_path.mkpath
-    module_path.install "src"
+    (etc/target).mkpath
+    (etc/target).install "src"
 
     bin.install "#{target}"
     man1.install "man/#{target}.1.gz"
