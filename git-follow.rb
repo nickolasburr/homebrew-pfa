49853849d764af32be875321d488b666d646aa26
Nickolas Burr <nickolasburr@gmail.com>
2018-01-29 10:32:05 -0500

    Update checksum for version 1.1.5

--- git-follow.rb
+++ git-follow.rb
@@ -2,8 +2,8 @@ class GitFollow < Formula
   desc "Follow lifetime changes of a pathspec in Git."
   homepage "https://github.com/nickolasburr/git-follow"
   url "https://github.com/nickolasburr/git-follow/archive/1.1.5.tar.gz"
-  sha256 "d71bf48e92d4dc37505cd5427fc7fa2fcb9b4f0604e4ad593c10252675934dcb"
-  revision 2
+  sha256 "2618d3a2437c3c85dc018c1ef6ddba7ef9040cf94fbab8d0e088e2497918c575"
+  revision 3
 
   def install
     target = "git-follow"
