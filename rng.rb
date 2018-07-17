c348fce63dd444bdc807c0af84b111c3c89a9bd5
Nickolas Burr <nickolasburr@gmail.com>
2018-07-17 14:22:00 -0400

    Update rng formula

--- rng.rb
+++ rng.rb
@@ -2,7 +2,8 @@ class Rng < Formula
   desc "Copy range of lines from file or stdin to stdout."
   homepage "https://github.com/nickolasburr/rng"
   url "https://github.com/nickolasburr/rng/archive/1.0.0.tar.gz"
-  sha256 "cebc32cc492b50355f99c8dc6ccb4c779f10b963ff4b2c4d7523447e4cb7e8d4"
+  sha256 "df11af038bec750f657008cddcc7ca8a54d9ba149977de23dd08c96bd57cacfb"
+  revision 1
 
   def install
     system "make"
